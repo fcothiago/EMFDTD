f621c5a8e737eb86f17003f85bb6e8be933fe22f
fcothiago <f.t.s.goncalves@gmail.com>
2023-04-08 14:07:38 -0300

    Bug in grid borders fixed

--- include/utils.h
+++ include/utils.h
@@ -8,9 +8,9 @@
 #define mu_0 (4*const_pi*1e-7)
 #define c_0 (1/sqrt(eps_0*mu_0))
 
-#define REMAINDER(u,l) ( u >= 0 ? u % l : l - ((-u) % l ) ) 
+#define REMAINDER(u,l) ( u >= 0 ? (u) % (l) : l - ((-u) % l ) ) 
 
-#define XY_TO_INDEX(x,y,nx,ny) ( REMAINDER(x,nx) + REMAINDER(y,ny)*nx )
+#define XY_TO_INDEX(x,y,nx,ny) ( REMAINDER(x,nx) + REMAINDER(y,ny)*(nx) )
 #define INDEX_TO_X(index,nx) ( REMAINDER(index,nx) )
 #define INDEX_TO_Y(index,nx) ( index/nx )
 
